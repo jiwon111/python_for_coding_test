a4c1a113816a9217a67d546a46aee87f5b404603
jiwon111 <dreamjiwon@naver.com>
2021-04-27 00:52:35 +0900

    clock

---
+++ chapter04/1-2.py
@@ -0,0 +1,9 @@
+n = int(input())
+cnt = 0
+
+for h in range(0, n+1):
+    for m in range(0, 60):
+        for s in range(0, 60):
+            if '3' in str(h)+str(m)+str(s):
+                cnt+=1
+print(cnt)
