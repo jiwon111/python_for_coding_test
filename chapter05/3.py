d9a70daac2580217c9b937254a0502298487da57
jiwon111 <dreamjiwon@naver.com>
2021-04-28 19:49:31 +0900

    update 5-10

---
+++ chapter05/3.py
@@ -0,0 +1,26 @@
+n, m = map(int, input().split())
+
+graph=[]
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+def dfs(x, y):
+    if x<=-1 or y<=-1 or x>=n or y>=m:
+        return 0
+
+    if graph[x][y] == 0:
+        graph[x][y]=1
+        dfs(x-1, y)
+        dfs(x, y - 1)
+        dfs(x+1, y)
+        dfs(x, y+1)
+        return 1
+    return 0
+
+cnt = 0
+for i in range(n):
+    for j in range(m):
+        if dfs(i, j) == 1:
+            cnt+=1
+
+print(cnt)
