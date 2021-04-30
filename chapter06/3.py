36108dd25313dd119a642642418c386223cf4230
jiwon111 <dreamjiwon@naver.com>
2021-05-01 00:46:44 +0900

    student

--- chapter06/3.py
+++ chapter06/3.py
@@ -6,7 +6,7 @@ for i in range(n):
     #이름은 문자열 그대로, 점수는 정수형으로 변환
     array.append((input_data[0], int(input_data[1])))
 
-array = sorted(array, key = lambda student: student[1])#점수를 기준으로 정렬
+array = sorted(array, key = lambda student: student[1]) #점수를 기준으로 정렬
 
 for student in array:
     print(student[0], end=' ')
