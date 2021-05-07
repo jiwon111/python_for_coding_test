28d2271c04849648f545e20005d3619098417cfc
jiwon111 <dreamjiwon@naver.com>
2021-05-07 15:05:51 +0900

    binary search

--- chapter07/3.py
+++ chapter07/3.py
@@ -2,5 +2,18 @@ n, m = map(int, input().split())
 array = list(map(int, input().split()))
 
 start = 0
-end = max(array)
-h = (start+end)//2
+end = max(array)#19
+
+result = 0
+while (start<=end):
+    total = 0
+    mid = (start+end)//2#9
+    for x in array:
+        if x>mid:
+            total=total+x-mid
+    if total<m:
+            end = mid-1
+    else:
+            result=mid
+            start=mid+1
+print(result)
