5542d2ec980d3903aa183ac201803f955e242459
jiwon111 <dreamjiwon@naver.com>
2021-05-18 22:26:54 +0900

    greedy

--- chapter11/2.py
+++ chapter11/2.py
@@ -2,7 +2,7 @@ s = input()
 result = int(s[0])
 
 for i in range(len(s) - 1):
-    if int(s[i]) == 0:
+    if int(s[i]) == 0 or int(s[i] == 1):
         result+=int(s[i+1])
     else:
         result*=int(s[i+1])
