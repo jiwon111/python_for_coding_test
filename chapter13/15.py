23b9e4da2ab9d8e5e4cbbd89733eeb18ccdc9fef
jiwon111 <dreamjiwon@naver.com>
2021-05-24 21:56:54 +0900

    bfs

--- chapter13/15.py
+++ chapter13/15.py
@@ -1,21 +1,19 @@
 from collections import deque
 
 n, m, k, x = map(int, input().split())
+graph=[[]for i in range(n+1)]
+distance = [-1]*(n+1)
+distance[x] = 0
 
-graph=[[] for i in range(n+1)]
 for i in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
 
-distance=[-1]*(n+1)
-distance[x] = 0
+q = deque()
 
-q = deque([x])
 while q:
     now = q.popleft()
-
     for next_node in graph[now]:
-        #아직 방문하지 않은 도시라면
         if distance[next_node] == -1:
             distance[next_node] = distance[now]+1
             q.append(next_node)
