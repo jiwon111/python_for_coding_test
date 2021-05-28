3b3916a6184ca0a75a58896424409bafe8105b58
jiwon111 <dreamjiwon@naver.com>
2021-05-28 21:49:19 +0900

    sorted

---
+++ chapter14/25.py
@@ -0,0 +1,20 @@
+n = int(input())
+stages = list(map(int, input().split()))
+
+result = []
+length = len(stages)
+for i in range(1, n+1):
+    cnt = stages.count(i)
+
+    if length == 0:
+        fail = 0
+    else:
+        fail = cnt/length
+
+    length-=cnt
+    result.append((i, fail))
+
+result = sorted(result, key=lambda t:t[1], reverse = True)
+print(result)
+result = [i[0] for i in result]
+print(result)
