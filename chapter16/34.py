e4b331b670e9f7ad264f861ccb24318b8d7b874a
jiwon111 <dreamjiwon@naver.com>
2021-06-01 21:03:24 +0900

    dynamic programming

---
+++ chapter16/34.py
@@ -0,0 +1,12 @@
+n = int(input())
+array = list(map(int, input().split()))
+
+array.reverse()
+dp = [1]*n
+
+for i in range(1, n):
+    for j in range(0, i):
+        if array[j] < array[i]:
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(n - max(dp))#열외시켜야 하는 병사의 최소 수
